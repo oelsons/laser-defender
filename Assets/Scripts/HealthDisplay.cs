178bea76be7e13c3c632f74c3b8e01ed2d2b270b
oelsons <oelsonseg@hotmail.com>
2019-05-28 15:37:18 -0300

    Laser Defender complete

---
+++ Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    Text healthText;
+    Player player;
+
+    private void Start()
+    {
+        healthText = GetComponent<Text>();
+        player = FindObjectOfType<Player>();
+    }
+
+    private void Update()
+    {
+        healthText.text = Mathf.Max(player.GetHealth(), 0).ToString();
+    }
+}
---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    Text scoreText;
+    GameSession gameSession;
+
+    private void Start()
+    {
+        scoreText = GetComponent<Text>();
+        gameSession = FindObjectOfType<GameSession>();
+    }
+
+    private void Update()
+    {
+        scoreText.text = gameSession.GetScore().ToString();
+    }
+}
